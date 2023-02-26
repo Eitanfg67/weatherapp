ce7ad2387363481614862c5c9c58fa1133e6691f
Eitanfg67 <125140796+Eitanfg67@users.noreply.github.com>
2023-02-26 14:29:59 +0200

    Add files via upload

---
+++ Script1.js
@@ -0,0 +1,62 @@
+summery = 0;
+
+function zero() {
+
+    document.getElementById("zero").innerHTML = summery = summery * 10 + 0;
+
+}
+function one() {
+
+    document.getElementById("zero").innerHTML = summery = summery * 10 + 1;
+
+}
+function two() {
+
+    document.getElementById("zero").innerHTML = summery = summery * 10 + 2;
+
+}
+function three() {
+
+    document.getElementById("zero").innerHTML = summery = summery * 10 + 3;
+
+}
+function four() {
+
+    document.getElementById("zero").innerHTML = summery = summery * 10 + 4;
+
+}
+function five() {
+
+    document.getElementById("zero").innerHTML = summery = summery * 10 + 5;
+
+}
+function six() {
+
+    document.getElementById("zero").innerHTML = summery = summery * 10 + 6;
+
+}
+function seven() {
+
+    document.getElementById("zero").innerHTML = summery = summery * 10 + 7;
+
+}
+function eight() {
+
+    document.getElementById("zero").innerHTML = summery = summery * 10 + 8;
+
+}
+function nine() {
+
+    document.getElementById("zero").innerHTML = summery = summery * 10 + 9;
+
+}
+function AC() {
+
+    document.getElementById("zero").innerHTML = summery = 0;
+
+}
+function dot() {
+
+    document.getElementById("zero").innerHTML = summery = summery + ".";
+
+}
